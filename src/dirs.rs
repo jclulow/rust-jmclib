1ada4e7654ef2b65b5c536eea7bf4320e2381363
Joshua M. Clulow <josh@sysmgr.org>
2020-03-15 02:25:44 +0000

    add "rootpath()" function

--- src/dirs.rs
+++ src/dirs.rs
@@ -1,4 +1,4 @@
-use std::path::PathBuf;
+use std::path::{Path, PathBuf};
 use std::path::Component;
 use std::ffi::OsStr;
 use std::io::Result;
@@ -13,6 +13,21 @@ fn pc(s: &str) -> Component {
     Component::Normal(OsStr::new(s))
 }
 
+fn ioe<T, S: AsRef<str>>(s: S) -> Result<T> {
+    Err(std::io::Error::new(std::io::ErrorKind::Other, s.as_ref()))
+}
+
+/**
+ * Obtain a fully qualified path to the project root directory.
+ *
+ * If the program is running from a Cargo target directory, e.g.,
+ * "/code/blah/target/release/program", then the project root directory is
+ * "/code/blah".
+ *
+ * If running from an apparent deployed location, e.g.,
+ * "/opt/software/bin/program", then the project root directory is one level up
+ * from "bin", e.g., "/opt/software".
+ */
 pub fn rootdir() -> Result<PathBuf> {
     let mut exe = std::env::current_exe()?;
 
@@ -43,7 +58,26 @@ pub fn rootdir() -> Result<PathBuf> {
         }
     }
 
-    Err(std::io::Error::new(std::io::ErrorKind::Other,
-        format!("could not determine data directory relative to executive \
-        path \"{}\"", exe.display())))
+    ioe(format!("could not determine data directory relative to executive \
+        path \"{}\"", exe.display()))
+}
+
+/**
+ * Obtain a fully qualified path to a project root-relative path.
+ *
+ * For example, if the program is "/opt/software/bin/program", and the provided
+ * path is "etc/config.toml", the result will be
+ * "/opt/software/etc/config.toml".
+ *
+ * See rootdir() for more details on project root directories.
+ */
+pub fn rootpath<P: AsRef<Path>>(p: P) -> Result<PathBuf> {
+    let p = p.as_ref();
+    if !p.is_relative() {
+        return ioe(format!("path \"{}\" is not relative", p.display()));
+    }
+
+    let mut path = rootdir()?;
+    path.push(p);
+    Ok(path)
 }
