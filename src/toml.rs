91b647f2168b1201567da694cbef898c72c2e472
Joshua M. Clulow <josh@sysmgr.org>
2020-06-01 15:21:08 +0000

    don't use Box<Error> in the API

--- src/toml.rs
+++ src/toml.rs
@@ -1,12 +1,50 @@
 use std::io::{Write, Read, BufReader, BufWriter, ErrorKind};
 use std::fs::File;
-use std::path::Path;
+use std::path::{Path, PathBuf};
+use std::error::Error;
 
 use serde::{Serialize, Deserialize};
 
 use tempfile::NamedTempFile;
 
-type Result<T> = std::result::Result<T, Box<dyn std::error::Error>>;
+type Result<T> = std::result::Result<T, TomlError>;
+
+#[derive(Debug)]
+pub struct TomlError {
+    msg: String,
+    path: PathBuf,
+}
+
+impl std::fmt::Display for TomlError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "TOML file \"{}\": {}", self.path.display(), self.msg)
+    }
+}
+
+impl Error for TomlError {
+}
+
+impl<T, E> Context<T, E> for std::result::Result<T, E>
+    where E: Error
+{
+    fn context(self, path: &Path) -> Result<T> {
+        self.map_err(|e| TomlError {
+            path: path.to_path_buf(),
+            msg: e.to_string(),
+        })
+    }
+}
+
+pub trait Context<T, E> {
+    fn context(self, path: &Path) -> Result<T>;
+}
+
+fn e(msg: &str, path: &Path) -> TomlError {
+    TomlError {
+        path: path.to_path_buf(),
+        msg: msg.to_string(),
+    }
+}
 
 /**
  * Read a TOML-formatted file into an optional object via serde deserialisation.
@@ -16,19 +54,21 @@ pub fn read_file<T, P: AsRef<Path>>(p: P) -> Result<Option<T>>
 where
     for<'de> T: Deserialize<'de>
 {
+    let p = p.as_ref();
+
     let f = match File::open(p) {
         Err(e) => match e.kind() {
             ErrorKind::NotFound => return Ok(None),
-            _ => return Err(e.into()),
+            _ => return Err(e).context(p),
         }
         Ok(f) => f,
     };
     let mut r = BufReader::new(f);
     let mut buf = Vec::<u8>::new();
 
-    r.read_to_end(&mut buf)?;
+    r.read_to_end(&mut buf).context(p)?;
 
-    Ok(Some(toml::from_slice(&buf)?))
+    Ok(Some(toml::from_slice(&buf).context(p)?))
 }
 
 /**
@@ -39,7 +79,7 @@ where
     T: Serialize
 {
     let p = p.as_ref();
-    let o = toml::to_vec(o)?;
+    let o = toml::to_vec(o).context(p)?;
 
     /*
      * In order to safely and atomically update the file with the new contents,
@@ -47,12 +87,12 @@ where
      * have damaged the original.
      */
     let dir = p.parent().expect("no parent directory?!");
-    let tf = NamedTempFile::new_in(dir)?;
+    let tf = NamedTempFile::new_in(dir).context(p)?;
 
     {
         let mut w = BufWriter::new(tf.as_file());
-        w.write_all(&o)?;
-        w.flush()?;
+        w.write_all(&o).context(p)?;
+        w.flush().context(p)?;
     }
 
     #[cfg(unix)]
@@ -64,7 +104,7 @@ where
         use std::os::unix::io::AsRawFd;
         let fd = tf.as_raw_fd();
         if unsafe { libc::fsync(fd) } != 0 {
-            return Err("fsync failure".into());
+            return Err(e("fsync failure", p));
         }
     }
 
@@ -74,6 +114,6 @@ where
      * XXX We should probably fsync(2) the directory after we perform the
      * rename.
      */
-    tf.persist(p)?;
+    tf.persist(p).context(p)?;
     Ok(())
 }
